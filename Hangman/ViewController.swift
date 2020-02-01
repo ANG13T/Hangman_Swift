2aac24e5eecd1f283a9178b85497c2055a2a471d
angelina-tsuboi <angelina.t1832@gmail.com>
2020-02-01 05:36:32 -0800

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Angelina Tsuboi on 2/1/20.
+//  Copyright © 2020 Angelina Tsuboi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
